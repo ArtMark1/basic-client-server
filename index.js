4d407a8d1937049e9595dac7ffa7e5b9d7a64e99
ArtMark1 <zuevartur777@gmail.com>
2023-08-13 17:06:28 +0200

    add style.css and add script.js

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ function handleRequest(request, response) {
   if (request.url == "/favicon.ico") {
     const icon = readFileSync("public/favicon.ico");
     response.end(icon);
+  } else if (request.url == "/" || request.url == "/index.html") {
+    const html = readFileSync("public/index.html");
+    response.end(html);
+  } else if (request.url == "/style.css") {
+    const css = readFileSync("public/style.css");
+    response.end(css);
+  } else if (request.url == "/script.js") {
+    const js = readFileSync("public/script.js");
+    response.end(js);
   } else {
     response.end("hello client");
   }
