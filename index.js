2358daba5b510796b6c15a0a10240266ae0a7225
ArtMark1 <zuevartur777@gmail.com>
2023-08-13 15:25:59 +0200

    add favicon and simple response

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const createServer = require("http");
+// const createServer = require("http").createServer;
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
+
 startServer();
 
 function startServer() {
   const server = createServer();
   const port = 1234;
+
   server.listen(port);
+
+  server.addListener("request", handleRequest);
+}
+
+function handleRequest(request, response) {
+  if (request.url == "/favicon.ico") {
+    const icon = readFileSync("favicon.ico");
+    response.end(icon);
+  } else {
+    response.end("hello client");
+  }
+  console.log("request: ", request.url);
 }
